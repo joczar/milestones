034d0d08897b28dddcfea23ea74d7ded925fc670
joczar <jczarnec@outlook.com>
2026-03-01 21:02:36 +0100

    feat: init view model

--- app/src/main/java/io/github/joczar/milestones/data/repository/GoalRepository.kt
+++ app/src/main/java/io/github/joczar/milestones/data/repository/GoalRepository.kt
@@ -2,8 +2,17 @@ package io.github.joczar.milestones.data.repository
 
 import io.github.joczar.milestones.data.dao.GoalDao
 import io.github.joczar.milestones.data.dao.StepDao
+import io.github.joczar.milestones.data.model.Goal
+import io.github.joczar.milestones.data.model.Step
+import kotlinx.coroutines.flow.Flow
 
 class GoalRepository(
     private val goalDao: GoalDao,
     private val stepDao: StepDao
-)
+) {
+    fun getAllGoals(): Flow<List<Goal>> = goalDao.fetchAll()
+
+    suspend fun insertGoal(goal: Goal) = goalDao.insert(goal)
+
+    fun getStepsForGoal(goalId: Long): Flow<List<Step>> = stepDao.findByGoalId(goalId)
+}
